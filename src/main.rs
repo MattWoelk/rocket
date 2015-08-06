3c0786b29b0b6ccfe0838d1327de703dbafda603
Benjamin Bergman <ben@benbergman.ca>
2015-08-05 22:09:48 -0500

    Slimmer repro code

--- src/main.rs
+++ src/main.rs
@@ -5,67 +5,22 @@ extern crate opengl_graphics;
 extern crate piston;
 extern crate rand;
 
-mod drawing;
-mod game;
-mod models;
-mod traits;
-
 use glutin_window::GlutinWindow;
 use opengl_graphics::{GlGraphics, OpenGL};
-use piston::event::{Event, Events, EventLoop, RenderEvent};
-use piston::input::{Button, Input};
+use piston::event::{Event, Events, EventLoop};
 use piston::window::WindowSettings;
 
-use drawing::Size;
-use game::Game;
+use std::path::Path;
+use graphics::{Context, Polygon};
+use graphics::math::Vec2d;
+use graphics::{Transformed};
+use opengl_graphics::glyph_cache::GlyphCache;
 
-extern crate sdl2;
-use sdl2::event::Event as SDL2Event;
-use sdl2::{joystick, controller};
-use sdl2::controller::GameController;
-
+use std::f64;
+const TAU: f64 = f64::consts::PI * 2.;
 
 fn main() {
     // Initialization stuff
-
-    let mut sdl_context = sdl2::init().game_controller().unwrap();
-    let available =
-        match joystick::num_joysticks() {
-            Ok(n)  => n,
-            Err(e) => panic!("can't enumerate joysticks: {}", e),
-        };
-
-    println!("{} joysticks available", available);
-
-    let mut controller = None;
-
-    // Iterate over all available joysticks and look for game
-    // controllers.
-    for id in 0..available {
-        if controller::is_game_controller(id) {
-            println!("Attempting to open controller {}", id);
-
-            match GameController::open(id) {
-                Ok(c) => {
-                    // We managed to find and open a game controller,
-                    // exit the loop
-                    println!("Success: opened \"{}\"", c.name());
-                    println!("Controller mapping: {}", c.mapping());
-                    controller = Some(c);
-                    break;
-                },
-                Err(e) => println!("failed: {:?}", e),
-            }
-
-        } else {
-             println!("{} is not a game controller", id);
-        }
-    }
-
-    if let None = controller {
-        println!("Couldn't open any controller");
-    }
-
     let opengl = OpenGL::_3_2;
 
     let window: GlutinWindow =
@@ -77,70 +32,98 @@ fn main() {
 
     let mut gl = GlGraphics::new(opengl);
 
-    // The game object
-    let mut game = Game::new(Size::new(1024.0, 600.0));
-
     // Event handling
     for e in window.events().ups(60).max_fps(60) {
         match e {
-            Event::Input(Input::Press(Button::Keyboard(key))) => {
-                game.key_press(key);
-            }
-
-            Event::Input(Input::Release(Button::Keyboard(key))) => {
-                game.key_release(key);
-            }
-
             Event::Render(args) => {
-                gl.draw(args.viewport(), |c, g| game.render(c, g));
-            }
-
-            Event::Update(args) => {
-                for sdl2_event in sdl_context.event_pump().poll_iter() {
-                    match sdl2_event {
-                        SDL2Event::ControllerAxisMotion{ axis, value: val, .. } => {
-                            game.handle_axis(axis, val as i32);
-                        }
-                        SDL2Event::ControllerButtonDown{ button, .. } => {
-                            match button {
-                                sdl2::controller::Button::A => {
-                                    game.key_press(piston::input::Key::Space);
-                                }
-                                sdl2::controller::Button::RightShoulder => {
-                                    game.key_press(piston::input::Key::Up);
-                                }
-                                sdl2::controller::Button::LeftShoulder => {
-                                    game.key_press(piston::input::Key::Up);
-                                }
-                                _ => {
-                                    println!("Button {:?} down", button)
-                                }
-                            }
-                        },
-                        SDL2Event::ControllerButtonUp{ button, .. } => {
-                            match button {
-                                sdl2::controller::Button::A => {
-                                    game.key_release(piston::input::Key::Space);
-                                }
-                                sdl2::controller::Button::RightShoulder => {
-                                    game.key_release(piston::input::Key::Up);
-                                }
-                                sdl2::controller::Button::LeftShoulder => {
-                                    game.key_release(piston::input::Key::Up);
-                                }
-                                _ => {
-                                    println!("Button {:?} down", button)
-                                }
-                            }
-                        }
-                        SDL2Event::Quit{..} => break,
-                        _ => (),
-                    }
-                }
-                game.update(args.dt);
+                gl.draw(args.viewport(), |c, g| draw_polygon(&c, g));
             }
 
             _ => {}
         }
+    }
+}
+
+
+fn draw_polygon(c: &Context, gl: &mut GlGraphics) {
+    let angle_segments = vec![[TAU * 1./6., TAU * 1./3.]];
+    let sides_per_one_radian = 32. / TAU;
+    let radius = 300.0;
+    let position = Point::new(300.0, 300.0);
+
+    for segment in &angle_segments {
+        let angle_indices_in_range = ((segment[1] - segment[0]) * sides_per_one_radian) as i64;
+        let range_of_angle_indices = 0..(angle_indices_in_range + 1);
+        let angles = range_of_angle_indices.map(|x| x as f64 * (segment[1] - segment[0]) / angle_indices_in_range as f64 + segment[0]);
+
+        let angles_vec = angles.collect::<Vec<_>>();
+
+        let outer_points = angles_vec.iter().map(
+            |&angle| {
+                Point::new_by_radius_angle(radius, angle).translate(&position)
+            });
+
+        let inner_points = angles_vec.iter().map(
+            |&angle| {
+                Point::new_by_radius_angle(radius - 40., angle).translate(&position)
+            }).rev();
+
+        let all_points = outer_points.chain(inner_points);
+
+        let vertices: Vec<Vec2d> = all_points
+            .map(|p| Vec2d::from(p))
+            .collect();
+
+
+        Polygon::new([0.0, 0.5, 0.0, 1.0])
+            .draw(&vertices,
+                  &c.draw_state,
+                  c.transform,
+                  gl);
+
+        for (i, p) in vertices.iter().enumerate() {
+            let mut text = graphics::Text::new(24);
+            text.color = [1.0, 0.5, 0.0, 1.0];
+            text.draw(&format!("{}", i),
+            &mut GlyphCache::new(&Path::new("resources/FiraMono-Bold.ttf")).unwrap(),
+            &c.draw_state,
+            c.trans(p[0], p[1]).transform,
+            gl);
+        }
+    }
+}
+
+
+/// A `Point` represents a position in space
+#[derive(Clone, Copy, Default, Debug)]
+pub struct Point {
+    pub x: f64,
+    pub y: f64
+}
+
+impl Point {
+    /// Returns a new `Point` with the given coordinates
+    pub fn new(x: f64, y: f64) -> Point {
+        Point { x: x, y: y}
+    }
+
+    pub fn new_by_radius_angle(radius: f64, angle:f64) -> Point {
+        Point {
+            x: radius * angle.sin(),
+            y: radius * angle.cos(),
+        }
+    }
+
+    /// Translates the point by another point
+    pub fn translate(mut self, other: &Point) -> Point {
+        self.x += other.x;
+        self.y += other.y;
+        self
+    }
+}
+
+impl From<Point> for Vec2d {
+    fn from(p: Point) -> Self {
+        [p.x, p.y]
     }
 }
