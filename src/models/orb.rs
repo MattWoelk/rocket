e9f2df6264784eb8bac9c5c25d7f2b1c2ad41be6
Matt Woelk <matt@mattwoelk.ca>
2015-10-06 20:46:00 -0500

    Proof of concept for storing closures in a struct.

--- src/models/orb.rs
+++ src/models/orb.rs
@@ -30,12 +30,49 @@ fn do_thing() {
 //
 //     println!("{:#?}", h);
 // }
+
+//fn do_hing(a:H, b:H) -> String {
+//    "STR".to_string()
+//}
 //
-// fn do_thing(a:H, b:H) -> String {
-//     "STR".to_string()
-// }
+//
+//struct H {
+//    f: (Fn(H, H) -> String),
+//}
 //
 //
-// struct H {
-//     f: (Fn(H, H) -> String),
-// }
+//
+//struct S<F> {
+//    f: F,
+//}
+
+
+
+
+fn foo<F>(mut hashfn: F)
+    where F : FnMut(&String) -> u64
+{
+    let x = format!("Foo");
+    let hash = hashfn(&x);
+}
+
+
+
+
+/// PROOF OF CONCEPT!
+pub fn main() {
+    let some_salt: String = "salty".to_string();
+    foo(|str| 4);
+
+    let mut g = G {
+        f: |str| 4,
+    };
+
+    println!("{}", (g.f)(&"staltly".to_string()));
+}
+
+struct G<F>
+    where F : FnMut(&String) -> u64
+{
+    f: F
+}
